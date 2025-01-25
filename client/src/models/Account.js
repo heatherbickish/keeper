250b78451a0704ff1eb7c6ba7c5a6bb73bc5331b
heather bickish <hbickish@gmail.com>
2025-01-25 12:31:43 -0700

    styled up keep card and connected to profile page

--- client/src/models/Account.js
+++ client/src/models/Account.js
@@ -1,12 +1,11 @@
 export class Account {
-  /**
-   * @param {{ id: string; email: string; name: string; picture: string; }} data
-   */
+
   constructor(data) {
     this.id = data.id;
     this.email = data.email;
     this.name = data.name;
     this.picture = data.picture;
+    this.coverImg = data.coverImg
     // TODO add additional properties if needed
   }
 }
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -6,6 +6,7 @@ import Pop from "@/utils/Pop";
 import { computed, onMounted } from "vue";
 
 const keeps = computed(() => AppState.keeps)
+const account = computed(() => AppState.account)
 
 onMounted(() => {
   getAllKeeps()
@@ -28,7 +29,14 @@ async function getAllKeeps() {
     <div class="col-md-12">
       <div class="masonry-container">
         <div v-for="keep in keeps" :key="keep.id" class="mb-3 mx-2">
-          <img :src="keep.img" :alt="'A picture of' + keep.name" class="picture-img rounded">
+          <div :style="{ backgroundImage: `url(${keep.img})` }" class="picture-img">
+            <div class="keep-info d-flex justify-content-between align-items-center p-3">
+              <h4>{{ keep.name }}</h4>
+              <router-link :to="{ name: 'Profile', params: { profileId: keep.creatorId } }">
+                <img :src="keep.creator.picture" :alt="'A picture of' + keep.creator.name" class="creator-img">
+              </router-link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -42,11 +50,37 @@ async function getAllKeeps() {
   >* {
     break-inside: avoid;
     display: inline-block;
+    width: 100%;
   }
 }
 
 .picture-img {
+  height: auto;
   width: 100%;
+  border-radius: 12px;
+  background-size: cover;
+  background-position: center;
   box-shadow: 0 5px 8px rgba(0, 0, 0, 0.2), 5px 6px 4px rgba(0, 0, 0, 0.16);
+  aspect-ratio: 1;
+  position: relative;
+}
+
+.creator-img {
+  height: 2.5rem;
+  border-radius: 50%;
+  border: 1px solid rgb(75, 72, 72);
+}
+
+.keep-info {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+h4 {
+  text-shadow: 1.3px 1.4px rgb(61, 59, 59);
+  font-weight: 600;
+  color: rgb(238, 233, 233);
 }
 </style>
